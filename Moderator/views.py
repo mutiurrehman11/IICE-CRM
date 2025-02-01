ebc2304d67a681829698aa3198678581ec729c26
mutiurrehman11 <callmemutiurrehman@gmail.com>
2025-02-02 01:53:18 +0500

    Profile Page

---
+++ Moderator/views.py
@@ -0,0 +1,389 @@
+import os
+from lib2to3.fixes.fix_input import context
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from authentication.models import User
+from Admin import models as admin_models
+from Admin.forms import UserForm, SessionForm, StudentForm, LeadForm
+from datetime import date
+
+
+def MakeNotification(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    sessions = admin_models.StudentSession.objects.all()
+    for session in sessions:
+        if session.due_date < date.today():
+            message = "Due Date passed for " + session.student.student_name + " in " + session.session.session_name + " session"
+            admin_models.Notification.objects.create(user=user, date=date.today(), category='Late fee',
+                                                     content=message)
+
+    notifications = admin_models.Notification.objects.all().order_by('-date')
+    context = {
+        'user': user,
+        'notifications': notifications
+    }
+    return render(request, 'Moderator/Notifications.html', context)
+def Notification(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    notifications = admin_models.Notification.objects.filter(category="Late fee").order_by('-date')
+    context = {
+        'user': user,
+        'notifications': notifications
+    }
+    return render(request, 'Moderator/Notifications.html', context)
+def DeleteStudentSession(request, studentsessionid):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    studentsession = admin_models.StudentSession.objects.get(id=studentsessionid)
+    studentid = studentsession.student.id
+    message = "Removed  " + studentsession.student.student_name + " from " + studentsession.session.session_name + " session"
+    admin_models.Notification.objects.create(user=user, date=date.today(), category='Deletion', content=message)
+    studentsession.delete()
+    return redirect('mod_StudentSession', studentid=studentid)
+def StudentSessionView(request, studentsessionid):
+    if 'user_id' not in request.session:
+        return redirect('home')
+
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    userdata = admin_models.StudentSession.objects.get(id=studentsessionid)  # The user you are trying to update
+
+    context = {
+        'user': user,
+        'userdata': userdata,
+    }
+
+    if request.method == 'POST':
+        userdata.status = request.POST.get('status')
+        userdata.notes = request.POST.get('notes')
+        userdata.due_date = request.POST.get('due_date')
+        message = "Updated  " + userdata.student.student_name + " Record in " + userdata.session.session_name + " session"
+        admin_models.Notification.objects.create(user=user, date=date.today(), category='Updation', content=message)
+        userdata.save()
+
+    return render(request, 'Moderator/StudentSessionView.html', context)
+def AddStudentSession(request, studentid):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    # Get the student object
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)
+    student =  admin_models.Student.objects.get(id=studentid)
+    active_sessions = admin_models.Sessions.objects.filter(status='Active')  # Fetch active sessions
+
+    if request.method == 'POST':
+        # Get form data
+        session_id = request.POST.get('session_id')
+        registration_fee = request.POST.get('registration_fee')
+        fee = request.POST.get('fee')
+        due_date = request.POST.get('due_date')
+        discount = request.POST.get('discount', 0)  # Default to 0 if no discount is provided
+        notes = request.POST.get('notes', '')
+
+        # Fetch the selected session object
+        session = admin_models.Sessions.objects.get(id=session_id)
+
+        # Create a new StudentSession instance
+        student_session = admin_models.StudentSession(
+            student=student,
+            session=session,
+            registration_date=request.POST.get('registration_date'),  # Set registration date if needed
+            registration_fee=registration_fee,
+            fee=fee,
+            due_date=due_date,
+            discount=discount,
+            fee_paid=0,
+            status='Active',  # Set initial status as Active
+            notes=notes,
+        )
+
+        # Save to the database
+        student_session.save()
+
+        message = "Added  " + student.student_name + " To " + session.session_name + " session"
+        admin_models.Notification.objects.create(user=user, date=date.today(), category='New Entry', content=message)
+
+        # Redirect to the desired page (e.g., student session view)
+        return redirect('mod_StudentSession', studentid=studentid)
+
+    # Render the form with student and active sessions
+    return render(request, 'Moderator/AddStudentSession.html', {
+        'student': student,
+        'active_sessions': active_sessions,
+    })
+def StudentSession(request, studentid):
+    if 'user_id' not in request.session:
+        return redirect('home')
+
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    userdata = admin_models.Student.objects.get(id=studentid)
+    sessions = admin_models.StudentSession.objects.filter(student=userdata)
+
+    context = {
+        'user': user,
+        'userdata': userdata,
+        'sessions': sessions,
+        'studentid': studentid
+    }
+    return render(request, 'Moderator/StudentSession.html', context)
+def LeadView(request, leadid):
+    if 'user_id' not in request.session:
+        return redirect('home')
+
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    userdata = admin_models.Lead.objects.get(id=leadid)  # The user you are trying to update
+
+    context = {
+        'user': user,
+        'userdata': userdata,
+    }
+
+    if request.method == 'POST':
+        form = LeadForm(request.POST, request.FILES, instance=userdata)  # Form for 'userdata'
+
+        if form.is_valid():
+            form.save()
+            message = "Updated  " + userdata.name + " Information in Leads"
+            admin_models.Notification.objects.create(user=user, date=date.today(), category='Deletion', content=message)
+            return redirect('mod_LeadView', leadid=leadid)  # Redirect to avoid resubmission
+        else:
+            # Print form errors for debugging
+            print("Form is not valid:")
+            print(form.errors)
+
+    else:
+        form = LeadForm(instance=userdata)
+
+    context['form'] = form
+    return render(request, 'Moderator/LeadView.html', context)
+def DeleteLead(request, leadid):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)
+    lead = admin_models.Lead.objects.get(id=leadid)
+    message = "Removed  " + lead.name + " from Leads"
+    admin_models.Notification.objects.create(user=user, date=date.today(), category='Deletion', content=message)
+    lead.delete()
+    return redirect('mod_Leads')
+def AddLead(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+
+        if form.is_valid():
+            newuser = form.save(commit=False)
+            newuser.save()  # Save the new user
+            message = "Added  " + newuser.name + " to Leads"
+            admin_models.Notification.objects.create(user=user, date=date.today(), category='New Entry', content=message)
+            messages.success(request, "Lead added successfully!")
+            return redirect('mod_Leads')
+        else:
+            print(form.errors)  # Debug: print any form errors
+
+    else:
+        form = LeadForm()
+
+    context = {
+        'user': user,
+        'form': form,
+    }
+    return render(request, 'Moderator/AddLead.html', context)
+def Leads(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')  # Get the logged-in user ID from the session
+    user = User.objects.get(id=user_id)  # Fetch the user object
+    Leads = admin_models.Lead.objects.all()
+    context = {
+        'user': user,
+        'Leads': Leads,
+    }
+    return render(request, 'Moderator/Leads.html', context)
+def DeleteStudent(request, studentid):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)
+    student = admin_models.Student.objects.get(id=studentid)
+    if student.profile_photo:
+        if os.path.exists(student.profile_photo.path):
+            os.remove(student.profile_photo.path)
+    if student.cnic_photo:
+        if os.path.exists(student.cnic_photo.path):
+            os.remove(student.cnic_photo.path)
+    if student.degree_photo:
+        if os.path.exists(student.degree_photo.path):
+            os.remove(student.degree_photo.path)
+    student.delete()
+    message = "Removed  " + student.student_name
+    admin_models.Notification.objects.create(user=user, date=date.today(), category='Deletion', content=message)
+    from_page = request.GET.get('from')
+    if from_page == 'exstudents':
+        return redirect('mod_ExStudents')  # Replace 'ExStudents' with the actual name of the ex-students page URL
+    return redirect('mod_Students')
+def StudentView(request, studentid):
+    if 'user_id' not in request.session:
+        return redirect('home')
+
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    userdata = admin_models.Student.objects.get(id=studentid)  # The user you are trying to update
+    status_choices = admin_models.Student.STATUS_CHOICES
+
+    context = {
+        'user': user,
+        'userdata': userdata,
+        'status_choices': status_choices,
+        'redirection': 1
+    }
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES, instance=userdata)  # Form for 'userdata'
+
+        if form.is_valid():
+            # Handle profile photo
+            if 'profile_photo' in request.FILES:
+                if userdata.profile_photo:
+                    print(f"Old profile photo path: {userdata.profile_photo.path}")
+                    if os.path.exists(userdata.profile_photo.path):
+                        os.remove(userdata.profile_photo.path)
+
+                userdata.profile_photo = request.FILES['profile_photo']
+            if 'cnic_photo' in request.FILES:
+                if userdata.cnic_photo:
+                    print(f"Old profile photo path: {userdata.cnic_photo.path}")
+                    if os.path.exists(userdata.cnic_photo.path):
+                        os.remove(userdata.cnic_photo.path)
+
+                userdata.cnic_photo = request.FILES['cnic_photo']
+            if 'degree_photo' in request.FILES:
+                if userdata.degree_photo:
+                    print(f"Old profile photo path: {userdata.degree_photo.path}")
+                    if os.path.exists(userdata.degree_photo.path):
+                        os.remove(userdata.degree_photo.path)
+
+                userdata.degree_photo = request.FILES['degree_photo']
+                    # Save user changes
+            form.save()
+            message = "Updated  " + userdata.student_name
+            admin_models.Notification.objects.create(user=user, date=date.today(), category='Updation', content=message)
+            return redirect('mod_StudentView', studentid=studentid)  # Redirect to avoid resubmission
+        else:
+            # Print form errors for debugging
+            print("Form is not valid:")
+            print(form.errors)
+
+    else:
+        form = StudentForm(instance=userdata)
+
+    context['form'] = form
+    return render(request, 'Moderator/StudentView.html', context)
+def ExStudents(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')  # Get the logged-in user ID from the session
+    user = User.objects.get(id=user_id)  # Fetch the user object
+    students = admin_models.Student.objects.filter(status="Inactive")
+    context = {
+        'user': user,
+        'students': students,
+        'redirection': 2
+    }
+    return render(request, 'Moderator/ExStudents.html', context)
+def AddStudent(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    print(user)
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            newuser = form.save(commit=False)
+
+            # Handle profile photo if uploaded
+            if 'profile_photo' in request.FILES:
+                newuser.profile_photo = request.FILES['profile_photo']
+            if 'cnic_photo' in request.FILES:
+                newuser.cnic_photo = request.FILES['cnic_photo']
+            if 'degree_photo' in request.FILES:
+                newuser.degree_photo = request.FILES['degree_photo']
+
+            newuser.save()  # Save the new user
+
+            message = "Added  " + newuser.student_name
+            admin_models.Notification.objects.create(user=user, date=date.today(), category='New Entry', content=message)
+            return redirect('mod_Students')
+        else:
+            print(form.errors)  # Debug: print any form errors
+
+    else:
+        form = StudentForm()
+
+    context = {
+        'user': user,
+        'form': form,
+    }
+    return render(request, 'Moderator/AddStudent.html', context)
+def Students(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')  # Get the logged-in user ID from the session
+    user = User.objects.get(id=user_id)  # Fetch the user object
+    students = admin_models.Student.objects.filter(status="Active")
+    context = {
+        'user': user,
+        'students': students,
+        'redirection': 1
+    }
+    return render(request, 'Moderator/Students.html', context)
+def Profile(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')  # Get the logged-in user ID from the session
+    user = User.objects.get(id=user_id)  # Fetch the user object
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES, instance=user)
+
+        if form.is_valid():
+            # Handle profile photo
+            if 'profile_photo' in request.FILES:
+                if user.profile_photo:
+                    print(user.profile_photo.path)
+                    # Delete old photo if it exists
+                    if os.path.exists(user.profile_photo.path):
+                        os.remove(user.profile_photo.path)
+
+                # Save the new photo
+                user.profile_photo = request.FILES['profile_photo']
+
+            # Save user changes
+            form.save()
+            return redirect('Moderator_Profile')  # Redirect to avoid form resubmission
+
+    else:
+        form = UserForm(instance=user)  # Pre-populate form with user data
+
+    return render(request, 'Moderator/Profile.html', {'form': form, 'user': user})
+
---
+++ Teacher/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from authentication.models import User
+from Admin import models as admin_models
+from datetime import date
+from Admin.forms import UserForm, SessionForm, StudentForm, LeadForm
+import os
+
+def select_course(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    courses = admin_models.Sessions.objects.all()
+    context = {
+        'user': user,
+        'courses': courses
+    }
+    return render(request, 'Teacher/Attendance.html', context)
+def mark_attendance(request, course_id):
+    if 'user_id' not in request.session:
+        return redirect('home')
+
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)  # Logged-in user
+    course = admin_models.Sessions.objects.get(id=course_id)
+    students = admin_models.StudentSession.objects.filter(session=course)
+
+    if request.method == 'POST':
+        date1 = request.POST.get('date')
+        for student in students:
+            status = request.POST.get(f'status_{student.student.id}')
+            admin_models.Attendance.objects.update_or_create(
+                course=course,
+                student=student.student,
+                date=date1,
+                defaults={'status': status}
+            )
+            sessions = admin_models.StudentSession.objects.filter(student=student.student)
+            for session in sessions:
+                if session.due_date < date.today():
+                    message = "Due Date passed for " + student.student.student_name + " in " + session.session.session_name + " session"
+                    admin_models.Notification.objects.create(user=user, date=date.today(), category='Late fee',
+                                                             content=message)
+        messages.success(request, 'Attendance marked successfully!')
+        return redirect('tec_select_course')
+    context = {
+        'user': user,
+        'course': course,
+        'students': students
+    }
+    return render(request, 'Teacher/Mark_Attendance.html', context)
+
+def Profile(request):
+    if 'user_id' not in request.session:
+        return redirect('home')
+    user_id = request.session.get('user_id')  # Get the logged-in user ID from the session
+    user = User.objects.get(id=user_id)  # Fetch the user object
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES, instance=user)
+
+        if form.is_valid():
+            # Handle profile photo
+            if 'profile_photo' in request.FILES:
+                if user.profile_photo:
+                    print(user.profile_photo.path)
+                    # Delete old photo if it exists
+                    if os.path.exists(user.profile_photo.path):
+                        os.remove(user.profile_photo.path)
+
+                # Save the new photo
+                user.profile_photo = request.FILES['profile_photo']
+
+            # Save user changes
+            form.save()
+            return redirect('Teacher_Profile')  # Redirect to avoid form resubmission
+
+    else:
+        form = UserForm(instance=user)  # Pre-populate form with user data
+
+    return render(request, 'Teacher/Profile.html', {'form': form, 'user': user})
