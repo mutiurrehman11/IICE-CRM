8d9ab0749f7d06b53a703ce986baea0ff2081e59
Muti Ur Rehman <callmemutiurrehman@gmail.com>
2025-08-23 01:23:48 +0500

    Update settings.py

--- IICE/settings.py
+++ IICE/settings.py
@@ -38,7 +38,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = ['iice-crm-5.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
